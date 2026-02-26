52daf0f248f391c23736cbd34cbb9d5cc1e8a3a1
OMI MATSUMOTO <oomi0704@users.noreply.github.com>
2026-02-27 06:04:58 +0900

    入力方法を確認

--- b/count_k.rb
+++ b/count_k.rb
@@ -1,14 +1,12 @@
-# 1行目: N と K を取得
-line1 = gets.split
-n = line1[0].to_i
-k = line1[1].to_i
-
-# 続く N 行: 配列 A の要素を取得
-a = []
-n.times do
-  a << gets.to_i
-end
-
-# 配列 A に含まれる K の数を数える
-count = a.count(k)
+# 1行目: "N K" を split で分割して N と K を取得
+input_line = gets.split
+n = input_line[0].to_i
+k = input_line[1].to_i
+  
+  a = []
+  n.times do 
+      a << gets.to_i
+  end 
+  
+  count = a.count(k)
 puts count
