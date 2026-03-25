b90b175ae0440d3cab89d9a121621cf88c018db1
OMI MATSUMOTO <oomi0704@users.noreply.github.com>
2026-03-25 22:04:06 +0900

    追加

---
+++ test/04.rb
@@ -0,0 +1,11 @@
+def chapter04(array)
+    # コードを記述
+    array.each do |i|
+        if i%5 == 0
+            i = 0
+        else
+            i
+        end    
+    end   
+end
+  p chapter04_05([1, 2, 3, 4, 5])
